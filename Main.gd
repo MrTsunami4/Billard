83a61a4521d9c8a5cc116ff82dd630da5f250850
Pierre Berthe <42767265+MrTsunami4@users.noreply.github.com>
2020-08-24 21:33:33 +0200

    Ajout marbre

--- Main.gd
+++ Main.gd
@@ -5,6 +5,7 @@ func _ready() -> void:
 
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("reload"):
+# warning-ignore:return_value_discarded
 		get_tree().reload_current_scene()
 	if Input.is_action_just_pressed("quit"):
 		get_tree().quit()
